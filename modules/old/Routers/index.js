9b0f4497658b40a2891ab0e5b96e87419bde466a
NuwanJ <nuwanjaliyagoda@gmail.com>
2021-03-03 17:23:57 +0530

    removed old files

--- modules/old/Routers/index.js
+++
@@ -1,9 +0,0 @@
-const { FIFORouter } = require('./FIFORouter');
-const { FQRouter } = require('./FQRouter');
-const { BasicRouter } = require('./BasicRouter');
-
-module.exports = {
-    BasicRouter,
-    FIFORouter,
-    FQRouter
-};
